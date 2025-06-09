1a28025d964855429b0f635d41011f018dc4bda5
KostoLabs <124069979+KostoLabs@users.noreply.github.com>
2025-06-09 22:16:55 +0000

    dotenv//prod

--- config/application.rb
+++ config/application.rb
@@ -2,7 +2,9 @@ require_relative "boot"
 
 require "rails/all"
 
-require "dotenv/load"
+if Rails.env.development? || Rails.env.test?
+  require "dotenv/load"
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
